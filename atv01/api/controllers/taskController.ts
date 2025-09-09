9e1d6666c7921416573b071803bbebf2fe6246c0
CauaGNP <163431893+CauaGNP@users.noreply.github.com>
2025-09-09 17:47:12 +0000

    Finsh aplication

---
+++ atv01/api/controllers/taskController.ts
@@ -0,0 +1,94 @@
+import { Request, Response } from "express";
+import { v4 as uuidV4 } from "uuid";
+import tasks from "@/models/Task.js";
+import { taskType } from "@/types/taskTypes.js";
+
+const getAllTasks = ({req, res}: {req: Request, res: Response}) => {
+    res.send({
+        data: tasks,
+    })
+}
+
+const getTaskById = ({req, res}: {req: Request, res: Response}) => {
+    const { taskId } = req.params;
+    
+    const task = tasks.find((e) => e.id === taskId);
+    if(!task){
+        res.status(404).send({
+            error: "Task not found"
+        })
+    }
+    res.status(200).send({
+        data: task,
+    });
+}
+
+const createTask = ({req, res}: {req: Request, res: Response}) => {
+    const newTask : taskType = {
+        id : uuidV4(),
+        description : req.body.description,
+        completed: false,
+    }
+
+    tasks.push(newTask);
+    res.send({
+        message: "Created task",
+        data: newTask, 
+    })
+}
+
+const updatedTask = ({req, res}: {req: Request, res: Response}) => {
+    const { taskId } = req.params;
+    
+    if(!taskId){
+        res.status(400).send({error: "Type error"})
+        return;
+    } 
+
+    const updatedTask : taskType = {
+        id : taskId,
+        description : req.body.description,
+        completed: req.body.completed,
+    }
+ 
+    tasks.forEach((e) => {
+        if (e.id === updatedTask.id) {
+            if (updatedTask.description !== undefined) {
+                e.description = updatedTask.description;
+            }
+            if (updatedTask.completed !== undefined) {
+                e.completed = updatedTask.completed;
+            }
+        }
+    });
+
+    res.status(200).send({
+        message: "Updated task",
+        data: updatedTask,
+    })
+}
+
+const deleteTask = ({req, res}: {req: Request, res: Response}) => {
+    const { taskId } = req.params;
+
+    const taskIndex = tasks.findIndex((e) => e.id === taskId);
+
+    if (taskIndex !== -1) {
+        tasks.splice(taskIndex, 1);
+        res.status(200).send({
+            message: "Deleted task!",
+        });
+    } else {
+        res.status(404).send({
+            message: "Error! Task not found.",
+        });
+    }
+}
+
+export {
+    getAllTasks,
+    getTaskById,
+    createTask,
+    updatedTask,
+    deleteTask
+}
--- atv01/api/routes/taskRoutes.ts
+++ atv01/api/routes/taskRoutes.ts
@@ -1,51 +1,12 @@
-import tasks from "@/models/Task.js";
-import { taskType } from "@/types/taskTypes.js";
 import { Router } from "express";
+import { createTask, deleteTask, getAllTasks, getTaskById, updatedTask } from "@/controllers/taskController.js"; 
 
 const router = Router();
 
-router.get("/:taskId", (req, res) => {});
-
-router.get("/", (req, res) => {
-  const task = tasks;
-
-  res.send({
-    data: task,
-  });
-});
-
-router.post("/", (req, res) => {});
-
-router.put("/:taskId", (req, res) => {
-  const taskAtt: taskType = {
-    id: req.params.taskId,
-    description: req.body.description,
-    completed: req.body.complete,
-  };
-
-  const task = tasks.find((e) => e.id === taskAtt.id);
-
-  if (!task) {
-    return res.status(404).json({ message: "Task não encontrada" });
-  }
-
-  taskAtt.description ? (task.description = taskAtt.description) : task;
-  taskAtt.completed ? (task.completed = taskAtt.completed) : task;
-
-  tasks.push(...tasks, taskAtt);
-});
-
-router.delete("/:taskId", (req, res) => {
-  const { taskId } = req.params;
-
-  const newTasksArray = tasks.filter((e) => e.id !== taskId);
-
-  tasks.length = 0;
-  tasks.push(...newTasksArray);
-
-  res.send({
-    message: "Task deleted successful!!",
-  });
-});
+router.get("/", getAllTasks);
+router.get("/:taskId", getTaskById);
+router.post("/", createTask);
+router.put("/:taskId", updatedTask);
+router.delete("/:taskId", deleteTask);
 
 export default router;
